3da1453e9a0b7051bfa2be40ad5abb3a074fd2a4
Patrick McCombs <pa.mccombs1@gmail.com>
2019-06-10 04:02:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = 
+const excitedWelcomeMessage = WELCOME TO FLATBOOK, 
